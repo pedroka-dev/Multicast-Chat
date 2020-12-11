9f8b0b76d8e56bcb1e9189f9a4184b19719c7209
pedro-ca <pedro.henric.rosario@gmail.com>
2020-12-11 16:20:41 -0300

    Updated interface and added frontend logic
    -Limit of 15 characters to Username and Address
    -No space allowed for Username and Adddress fields
    -Limit of 5 characters to Port
    -No characters other than numbers for Por field
    -Rearenged interface
    -Fixed tab index
    -Address, Port and Username are obligatory to login.
    -Message Chat tab becomes only avaliable after login
    -Added Use Encryption and Key Fields

--- MulticastChat/MulticastChat/FormMain.Designer.cs
+++ MulticastChat/MulticastChat/FormMain.Designer.cs
@@ -36,17 +36,23 @@
             this.textBoxPort = new System.Windows.Forms.TextBox();
             this.buttonJoin = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
-            this.labelMessageChat = new System.Windows.Forms.Label();
             this.listBoxMessageChat = new System.Windows.Forms.ListBox();
             this.labelNewMessage = new System.Windows.Forms.Label();
             this.textBoxNewMessage = new System.Windows.Forms.TextBox();
             this.buttonSend = new System.Windows.Forms.Button();
+            this.checkBoxUseEncryption = new System.Windows.Forms.CheckBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelUserName
             // 
             this.labelUserName.AutoSize = true;
-            this.labelUserName.Location = new System.Drawing.Point(9, 13);
+            this.labelUserName.Location = new System.Drawing.Point(15, 10);
             this.labelUserName.Name = "labelUserName";
             this.labelUserName.Size = new System.Drawing.Size(83, 17);
             this.labelUserName.TabIndex = 0;
@@ -55,127 +61,192 @@
             // 
             // textBoxUserName
             // 
-            this.textBoxUserName.Location = new System.Drawing.Point(102, 10);
+            this.textBoxUserName.Location = new System.Drawing.Point(104, 10);
+            this.textBoxUserName.MaxLength = 15;
             this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(311, 22);
+            this.textBoxUserName.Size = new System.Drawing.Size(135, 22);
             this.textBoxUserName.TabIndex = 1;
+            this.textBoxUserName.TextChanged += new System.EventHandler(this.textBoxUserName_TextChanged);
             // 
             // labelAddress
             // 
             this.labelAddress.AutoSize = true;
-            this.labelAddress.Location = new System.Drawing.Point(23, 47);
+            this.labelAddress.Location = new System.Drawing.Point(24, 35);
             this.labelAddress.Name = "labelAddress";
-            this.labelAddress.Size = new System.Drawing.Size(97, 17);
+            this.labelAddress.Size = new System.Drawing.Size(64, 17);
             this.labelAddress.TabIndex = 2;
-            this.labelAddress.Text = "Chat Address:";
+            this.labelAddress.Text = "Address:";
+            this.labelAddress.Click += new System.EventHandler(this.labelAddress_Click);
             // 
             // textBoxAddress
             // 
-            this.textBoxAddress.Location = new System.Drawing.Point(126, 47);
+            this.textBoxAddress.Location = new System.Drawing.Point(94, 30);
+            this.textBoxAddress.MaxLength = 15;
             this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(153, 22);
+            this.textBoxAddress.Size = new System.Drawing.Size(135, 22);
             this.textBoxAddress.TabIndex = 2;
+            this.textBoxAddress.TextChanged += new System.EventHandler(this.textBoxAddress_TextChanged);
             // 
             // labelPort
             // 
             this.labelPort.AutoSize = true;
-            this.labelPort.Location = new System.Drawing.Point(49, 82);
+            this.labelPort.Location = new System.Drawing.Point(24, 74);
             this.labelPort.Name = "labelPort";
-            this.labelPort.Size = new System.Drawing.Size(71, 17);
+            this.labelPort.Size = new System.Drawing.Size(38, 17);
             this.labelPort.TabIndex = 4;
-            this.labelPort.Text = "Chat Port:";
+            this.labelPort.Text = "Port:";
+            this.labelPort.Click += new System.EventHandler(this.labelPort_Click);
             // 
             // textBoxPort
             // 
-            this.textBoxPort.Location = new System.Drawing.Point(126, 82);
+            this.textBoxPort.Location = new System.Drawing.Point(94, 71);
+            this.textBoxPort.MaxLength = 5;
             this.textBoxPort.Name = "textBoxPort";
-            this.textBoxPort.Size = new System.Drawing.Size(153, 22);
+            this.textBoxPort.Size = new System.Drawing.Size(74, 22);
             this.textBoxPort.TabIndex = 3;
+            this.textBoxPort.TextChanged += new System.EventHandler(this.textBoxPort_TextChanged);
+            this.textBoxPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPort_KeyPress);
             // 
             // buttonJoin
             // 
-            this.buttonJoin.Location = new System.Drawing.Point(300, 46);
+            this.buttonJoin.Location = new System.Drawing.Point(18, 204);
             this.buttonJoin.Name = "buttonJoin";
-            this.buttonJoin.Size = new System.Drawing.Size(113, 23);
-            this.buttonJoin.TabIndex = 4;
+            this.buttonJoin.Size = new System.Drawing.Size(79, 36);
+            this.buttonJoin.TabIndex = 6;
             this.buttonJoin.Text = "Join Chat";
             this.buttonJoin.UseVisualStyleBackColor = true;
+            this.buttonJoin.Click += new System.EventHandler(this.buttonJoin_Click);
             // 
             // buttonExit
             // 
-            this.buttonExit.Location = new System.Drawing.Point(300, 82);
+            this.buttonExit.Enabled = false;
+            this.buttonExit.Location = new System.Drawing.Point(252, 204);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(113, 23);
-            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Size = new System.Drawing.Size(79, 36);
+            this.buttonExit.TabIndex = 7;
             this.buttonExit.Text = "Exit Chat";
             this.buttonExit.UseVisualStyleBackColor = true;
-            // 
-            // labelMessageChat
-            // 
-            this.labelMessageChat.AutoSize = true;
-            this.labelMessageChat.Location = new System.Drawing.Point(12, 122);
-            this.labelMessageChat.Name = "labelMessageChat";
-            this.labelMessageChat.Size = new System.Drawing.Size(100, 17);
-            this.labelMessageChat.TabIndex = 8;
-            this.labelMessageChat.Text = "Message chat:";
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
             // listBoxMessageChat
             // 
+            this.listBoxMessageChat.Enabled = false;
             this.listBoxMessageChat.FormattingEnabled = true;
             this.listBoxMessageChat.ItemHeight = 16;
             this.listBoxMessageChat.Items.AddRange(new object[] {
             " "});
-            this.listBoxMessageChat.Location = new System.Drawing.Point(15, 142);
+            this.listBoxMessageChat.Location = new System.Drawing.Point(15, 21);
             this.listBoxMessageChat.Name = "listBoxMessageChat";
-            this.listBoxMessageChat.Size = new System.Drawing.Size(398, 196);
-            this.listBoxMessageChat.TabIndex = 6;
+            this.listBoxMessageChat.Size = new System.Drawing.Size(298, 196);
+            this.listBoxMessageChat.TabIndex = 8;
             this.listBoxMessageChat.SelectedIndexChanged += new System.EventHandler(this.listBoxChat_SelectedIndexChanged);
             // 
             // labelNewMessage
             // 
             this.labelNewMessage.AutoSize = true;
-            this.labelNewMessage.Location = new System.Drawing.Point(9, 351);
+            this.labelNewMessage.Location = new System.Drawing.Point(12, 220);
             this.labelNewMessage.Name = "labelNewMessage";
             this.labelNewMessage.Size = new System.Drawing.Size(100, 17);
             this.labelNewMessage.TabIndex = 10;
             this.labelNewMessage.Text = "New message:";
+            this.labelNewMessage.Click += new System.EventHandler(this.labelNewMessage_Click);
             // 
             // textBoxNewMessage
             // 
-            this.textBoxNewMessage.Location = new System.Drawing.Point(12, 371);
+            this.textBoxNewMessage.Enabled = false;
+            this.textBoxNewMessage.Location = new System.Drawing.Point(15, 239);
             this.textBoxNewMessage.Name = "textBoxNewMessage";
-            this.textBoxNewMessage.Size = new System.Drawing.Size(314, 22);
-            this.textBoxNewMessage.TabIndex = 7;
+            this.textBoxNewMessage.Size = new System.Drawing.Size(213, 22);
+            this.textBoxNewMessage.TabIndex = 9;
             // 
             // buttonSend
             // 
-            this.buttonSend.Location = new System.Drawing.Point(346, 371);
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(249, 238);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(67, 23);
-            this.buttonSend.TabIndex = 8;
+            this.buttonSend.Size = new System.Drawing.Size(57, 23);
+            this.buttonSend.TabIndex = 9;
             this.buttonSend.Text = "Send";
             this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxUseEncryption
+            // 
+            this.checkBoxUseEncryption.AutoSize = true;
+            this.checkBoxUseEncryption.Location = new System.Drawing.Point(183, 73);
+            this.checkBoxUseEncryption.Name = "checkBoxUseEncryption";
+            this.checkBoxUseEncryption.Size = new System.Drawing.Size(126, 21);
+            this.checkBoxUseEncryption.TabIndex = 4;
+            this.checkBoxUseEncryption.Text = "Use Encryption";
+            this.checkBoxUseEncryption.UseVisualStyleBackColor = true;
+            this.checkBoxUseEncryption.CheckedChanged += new System.EventHandler(this.checkBoxUseEncryption_CheckedChanged);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Enabled = false;
+            this.textBoxKey.Location = new System.Drawing.Point(95, 112);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(214, 22);
+            this.textBoxKey.TabIndex = 5;
+            this.textBoxKey.TextChanged += new System.EventHandler(this.textBoxKey_TextChanged);
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(25, 112);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(36, 17);
+            this.labelKey.TabIndex = 14;
+            this.labelKey.Text = "Key:";
+            this.labelKey.Click += new System.EventHandler(this.labelKey_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelKey);
+            this.groupBox1.Controls.Add(this.labelAddress);
+            this.groupBox1.Controls.Add(this.textBoxKey);
+            this.groupBox1.Controls.Add(this.textBoxAddress);
+            this.groupBox1.Controls.Add(this.checkBoxUseEncryption);
+            this.groupBox1.Controls.Add(this.labelPort);
+            this.groupBox1.Controls.Add(this.textBoxPort);
+            this.groupBox1.Location = new System.Drawing.Point(11, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 152);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chat configurations";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonSend);
+            this.groupBox2.Controls.Add(this.textBoxNewMessage);
+            this.groupBox2.Controls.Add(this.labelNewMessage);
+            this.groupBox2.Controls.Add(this.listBoxMessageChat);
+            this.groupBox2.Location = new System.Drawing.Point(11, 246);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 270);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Message Chat";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 407);
-            this.Controls.Add(this.buttonSend);
-            this.Controls.Add(this.textBoxNewMessage);
-            this.Controls.Add(this.labelNewMessage);
-            this.Controls.Add(this.listBoxMessageChat);
-            this.Controls.Add(this.labelMessageChat);
+            this.ClientSize = new System.Drawing.Size(351, 528);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonJoin);
-            this.Controls.Add(this.textBoxPort);
-            this.Controls.Add(this.labelPort);
-            this.Controls.Add(this.textBoxAddress);
-            this.Controls.Add(this.labelAddress);
             this.Controls.Add(this.textBoxUserName);
             this.Controls.Add(this.labelUserName);
             this.Name = "FormMain";
             this.Text = "Multicast Chat";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,11 +262,15 @@
         private System.Windows.Forms.TextBox textBoxPort;
         private System.Windows.Forms.Button buttonJoin;
         private System.Windows.Forms.Button buttonExit;
-        private System.Windows.Forms.Label labelMessageChat;
         private System.Windows.Forms.Label labelNewMessage;
         private System.Windows.Forms.TextBox textBoxNewMessage;
         private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.ListBox listBoxMessageChat;
+        private System.Windows.Forms.CheckBox checkBoxUseEncryption;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- MulticastChat/MulticastChat/FormMain.cs
+++ MulticastChat/MulticastChat/FormMain.cs
@@ -26,5 +26,109 @@ namespace MulticastChat
         {
 
         }
+
+        private void textBoxPort_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxUseEncryption_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxUseEncryption.Checked)
+            {
+                textBoxKey.Enabled = true;
+            }
+            else
+            {
+                textBoxKey.Enabled = false;
+                textBoxKey.Clear();
+            }
+        }
+
+        private void labelNewMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxKey_TextChanged(object sender, EventArgs e)
+        {
+            textBoxKey.Text = textBoxKey.Text.Replace(" ", "");
+        }
+
+        private void labelPort_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonJoin_Click(object sender, EventArgs e)
+        {
+            if(textBoxUserName.Text.Length == 0 || textBoxAddress.Text.Length == 0 || textBoxPort.Text.Length == 0)
+            {
+                MessageBox.Show("Fields 'User Name', 'Address' and 'Port' cannot be blank.", "Invalid Arguments");
+            }
+            else
+            {
+                MessageBox.Show("Joined the Chat room.", "Login sucessfull");
+
+                //Deve testar conexao antes de fazer essa parte
+                buttonExit.Enabled = true;
+                listBoxMessageChat.Enabled = true;
+                textBoxNewMessage.Enabled = true;
+                buttonSend.Enabled = true;
+                buttonJoin.Enabled = false;
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            buttonExit.Enabled = false;
+            listBoxMessageChat.Enabled = false;
+            textBoxNewMessage.Enabled = false;
+            buttonSend.Enabled = false;
+            buttonJoin.Enabled = true;
+        }
+
+        private void labelKey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelMessageChat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxAddress_TextChanged(object sender, EventArgs e)
+        {
+            textBoxAddress.Text = textBoxAddress.Text.Replace(" ", "");
+        }
+
+        private void labelAddress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUserName_TextChanged(object sender, EventArgs e)
+        {
+            textBoxUserName.Text = textBoxUserName.Text.Replace(" ", "");
+        }
     }
 }
